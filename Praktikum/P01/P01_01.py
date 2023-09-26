bbc13e13173425d2e6196cfe0a0f7d61d4e55eee
Kurond <68522463+Kurosue@users.noreply.github.com>
2023-09-26 08:06:47 +0700

    Create P01_01.py

---
+++ Praktikum/P01/P01_01.py
@@ -0,0 +1,27 @@
+# NIM/NAMA   : 19623224/Muhammad Aditya Rahmadeni
+# Tanggal    : 21 September 2023
+# Deskripsi  : Program Konversi Mata Uang ke Rupiah
+
+# Kamus
+# Peng, Kom, CPeng, CKom, RPeng, RKom = integer
+# uang = string
+# 
+
+# Algoritma
+# Input
+Peng = int(input('Banyak uang Peng yang ditawarkan: '))
+Kom = int(input('Banyak uang Kom yang ditawarkan: '))
+CPeng = int(input('Konversi mata uang Peng ke rupiah: '))
+CKom = int(input('Konversi mata uang Kom ke rupiah: '))
+# Proses
+uang = ''
+RPeng = Peng * CPeng # Nilai peng ke rupiah 
+RKom = Kom * CKom # Nilai kom ke rupiah
+
+if RPeng > RKom:
+    uang = 'Peng'
+else: # RKom > RPeng
+    uang = 'Kom'
+
+# Output
+print(f'Adik Tuan Kil memilih uang {uang}.')
