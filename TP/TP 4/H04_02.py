419018f2901a3b90c97ad1021e25cf22fcc429d1
Kurond <68522463+Kurosue@users.noreply.github.com>
2023-10-24 19:09:14 +0700

    Update and rename H04_19623224_02.py to H04_02.py

--- TP/TP 4/H04_02.py
+++ TP/TP 4/H04_02.py
@@ -1,5 +1,3 @@
-# NIM/Nama  : 19623224/MUHAMMAD ADITYA RAHMADENI
-# Tanggal   : 23 Oktober 2023
 # Deksripsi : Program Menentukan Banyak Bakteri di-K dengan Jumlah Awal N
 
 # Kamus
@@ -11,18 +9,19 @@
 
 # Algoritma
 # Fungsi
-def hitung(N, K):
+def hitung(n, k):
     used = 0
     sum = N
-    temp = 0
     for i in range(1, K+1):
+        # Mengulang sebanyak K
+        # Mengkalikan bilangan yang baru dan ditambah dengan jumlah sebelumnya
         temp = sum
         sum = (sum - used)*2 + sum
-        used =  temp
+        used = temp
     return sum
 
 # Input
-N = int(input())
-K = int(input())
+N = int(input('Masukkan N: '))
+K = int(input('Masukkan K: '))
 # Output
-print(hitung(N,K))
+print(f'Terdapat {hitung(N,K)} Bakteri Pengkombacter.')
